f4c91f33bbd863fe2d2a6352b9f1ae4779c576f2
Alexandr Yanul <ady@infoservice.ru>
2022-09-04 00:57:50 +0300

    базовое представление теперь возвращает html

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,9 @@
+from turtle import ht
+from urllib import request, response
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
+from django.http import HttpRequest
 
 class HomePageTest(TestCase):
     '''Тест домашней страницы'''
@@ -9,3 +12,12 @@ class HomePageTest(TestCase):
         '''Тест: корневой url преобразуется в представление домашней страницы'''
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        '''Тест: домашняя страница возвращает правильный html'''
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
