a76ad791f06eaa492b393803d1d3b15195e036c4
Alexandr Yanul <ady@infoservice.ru>
2022-09-06 20:08:40 +0300

    Сделан вывод одного пункта списка

--- functional_tests.py
+++ functional_tests.py
@@ -42,17 +42,22 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertTrue(
-            any(row.text == '1: Купить павлиньи перья' for row in rows),
-            "Новый элемент списка не появился в таблице"
-        )
+        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
 
         # Текстовое поле по-прежнему приглашает ее добавить еще один элемент. Она вводит "Сделать мушку из павлиньих перьев"
-        self.fail('Дописать тест!')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Сделать мушку из павлиньих перьев')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # Страница снова обновляется. И теперь показывает оба элемента списка.
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
+        self.assertIn('2: Сделать мушку из павлиньих перьев', [row.text for row in rows])
 
         # Эдит интересно, запомнит ли сайт ее список. Далее она видит, что сайт сгенерировал для нее уникальный URL-адрес - об этом выводится небольшой текст с разъяснениями
+        self.fail('Дописать тест!')
 
         # Она посещает этот URL-адрес. Ее список по прежнему там
 
--- lists/tests.py
+++ lists/tests.py
@@ -1,20 +1,19 @@
-from turtle import ht
-from urllib import request, response
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
 
+
 class HomePageTest(TestCase):
-    '''Тест домашней страницы'''
+    """Тест домашней страницы"""
 
     def test_root_url_resolves_to_home_page_view(self):
-        '''Тест: корневой url преобразуется в представление домашней страницы'''
+        """Тест: корневой url преобразуется в представление домашней страницы"""
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        '''Тест: домашняя страница возвращает правильный html'''
+        """Тест: домашняя страница возвращает правильный html"""
         request = HttpRequest()
         response = home_page(request)
         html = response.content.decode('utf8')
@@ -23,5 +22,12 @@ class HomePageTest(TestCase):
         self.assertTrue(html.strip().endswith('</html>'))
 
     def test_uses_home_template(self):
+        """Тест: используется домашний шаблон"""
         response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_post_request(self):
+        """Тест: можно сохранить POST-запрос"""
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
