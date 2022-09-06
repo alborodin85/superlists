55e8ceb97abb32b97e969eed5cbae788f10008e6
Alexandr Yanul <ady@infoservice.ru>
2022-09-06 20:18:51 +0300

    Рефакторинг дублирования ФТ

--- functional_tests.py
+++ functional_tests.py
@@ -6,18 +6,24 @@ from selenium.webdriver.common.by import By
 
 
 class NewVisitorTest(unittest.TestCase):
-    '''Тест нового посетителя'''
+    """Тест нового посетителя"""
 
     def setUp(self) -> None:
-        '''Установка'''
+        """Установка"""
         self.browser = webdriver.Firefox()
 
     def tearDown(self) -> None:
-        '''Демонтаж'''
+        """Демонтаж"""
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        """Подтверждение строки в таблице списка"""
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
-        '''тест: можно начать список и получить его позже'''
+        """Тест: можно начать список и получить его позже"""
         # Эдит слышала про крутое онлайн-решение со списком неотложных дел. Она решает оценить домашнюю страницу.
         self.browser.get('http://localhost:8000')
 
@@ -27,34 +33,29 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', header_text)
 
         # Ей сразу же предлагается ввести элемент списка
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputBox = self.browser.find_element(By.ID, 'id_new_item')
         self.assertEqual(
-            inputbox.get_attribute('placeholder'),
+            inputBox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
 
         # Она набирает в текстовом поле "Купить павлиньи перья"
-        inputbox.send_keys('Купить павлиньи перья')
+        inputBox.send_keys('Купить павлиньи перья')
 
         # Когда она нажимает Enter, страница обновляется, и теперь страница содержит "1: Купить павлиньи перья"
-        inputbox.send_keys(Keys.ENTER)
+        inputBox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Купить павлиньи перья')
 
         # Текстовое поле по-прежнему приглашает ее добавить еще один элемент. Она вводит "Сделать мушку из павлиньих перьев"
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')
-        inputbox.send_keys('Сделать мушку из павлиньих перьев')
-        inputbox.send_keys(Keys.ENTER)
+        inputBox = self.browser.find_element(By.ID, 'id_new_item')
+        inputBox.send_keys('Сделать мушку из павлиньих перьев')
+        inputBox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         # Страница снова обновляется. И теперь показывает оба элемента списка.
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
-        self.assertIn('2: Сделать мушку из павлиньих перьев', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Купить павлиньи перья')
+        self.check_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
 
         # Эдит интересно, запомнит ли сайт ее список. Далее она видит, что сайт сгенерировал для нее уникальный URL-адрес - об этом выводится небольшой текст с разъяснениями
         self.fail('Дописать тест!')
