48e2d74849e0cf72be57d1dffee5e37226aa273e
Alexandr Yanul <ady@infoservice.ru>
2022-09-07 20:40:52 +0300

    сделана связь модели со списком

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
-from lists.models import Item
+from lists.models import Item, List
 
 def home_page(request):
     '''Домашняя страница'''
@@ -14,5 +14,6 @@ def view_list(request):
 
 def new_list(request):
     """Новый список"""
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/alon-in-orig-rode-list-in-word/')
