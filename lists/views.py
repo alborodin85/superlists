18030a7ad58cf5f832f605d793599cb4ad9823cc
Alexandr Yanul <ady@infoservice.ru>
2022-09-11 17:32:50 +0300

    Правки оформления

--- lists/views.py
+++ lists/views.py
@@ -3,15 +3,18 @@ from django.http import HttpResponse
 from lists.models import Item, List
 from django.core.exceptions import ValidationError
 
+
 def home_page(request):
     '''Домашняя страница'''
     return render(request, 'home.html')
+
 
 def view_list(request, list_id):
     """Представление списка"""
     list_ = List.objects.get(id=list_id)
 
     return render(request, 'list.html', {'list': list_})
+
 
 def new_list(request):
     """Новый список"""
@@ -25,6 +28,7 @@ def new_list(request):
         error = "You can't have an empty list item"
         return render(request, 'home.html', {"error": error})
     return redirect(f'/lists/{list_.id}/')
+
 
 def add_item(request, list_id):
     """Добавить элемент"""
